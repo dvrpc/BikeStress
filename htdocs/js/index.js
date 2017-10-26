733d3241e2d6ae8cda41c1dae9263e2e0f23bcf6
wtsay <wtsay@dvrpc.org>
2017-10-26 15:15:48 -0400

    Schiphol

--- htdocs/js/index.js
+++ htdocs/js/index.js
@@ -14,71 +14,98 @@ var paperUSLetterL = {
     name: 'Landscape'
 }
 
-var overlays = 
+var overlays = {
+    definition: [
+        {
+            group: "Guidance",
+            items: [
+                {
+                    label: "Regional Priorities - Philadelphia",
+                    URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_RegionalPrioritiesPhila/MapServer/tile/{z}/{y}/{x}"
+                },
+                {
+                    label: "Regional Priorities - Suburban Counties",
+                    URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_RegPrioritiesSuburban/MapServer/tile/{z}/{y}/{x}"
+                }
+            ]
+        },
+        {
+            group: "Source",
+            items: [
+                {
+                    label: "Paved Shoulders",
+                    URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_PavedShoulders/MapServer/tile/{z}/{y}/{x}"
+                },
+                {
+                    label: "Assigned LTS",
+                    URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/AssignedLTS/MapServer/tile/{z}/{y}/{x}"
+                }
+            ]
+        },
+        {
+            group: "Input",
+            items: [
+                {
+                    label: "LTS 1 and LTS 2 Islands",
+                    URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_LTS12Islands/MapServer/tile/{z}/{y}/{x}"
+                },
+                {
+                    label: "LTS 3 - Philadelphia County",
+                    URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_PhilaLTS3/MapServer/tile/{z}/{y}/{x}"
+                },
+                {
+                    label: "LTS 3 - Suburban Counties",
+                    URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_SuburbanLTS123/MapServer/tile/{z}/{y}/{x}"
+                }
+            ]
+        },
+        {
+            group: "Results",
+            items: [
+                {
+                    label: "Shortest Path - Philadelphia County",
+                    URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_PhilaShortestPath/MapServer/tile/{z}/{y}/{x}"
+                },
+                {
+                    label: "Shortest Path - Suburban Counties",
+                    URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_SuburbanShortestPath/MapServer/tile/{z}/{y}/{x}"
+                }
+            ]
+        },
+    ],
+    data: null
+}
 
-[
-    {
-        group: "Guidance",
-        items: [
-            {
-                label: "Regional Priorities - Philadelphia",
-                URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_RegionalPrioritiesPhila/MapServer/tile/{z}/{y}/{x}"
-            },
-            {
-                label: "Regional Priorities - Suburban Counties",
-                URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_RegPrioritiesSuburban/MapServer/tile/{z}/{y}/{x}"
-            }
-        ]
-    },
-    {
-        group: "Source",
-        items: [
-            {
-                label: "Paved Shoulders",
-                URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_PavedShoulders/MapServer/tile/{z}/{y}/{x}"
-            },
-            {
-                label: "Assigned LTS",
-                URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/AssignedLTS/MapServer/tile/{z}/{y}/{x}"
-            }
-        ]
-    },
-    {
-        group: "Input",
-        items: [
-            {
-                label: "LTS 1 and LTS 2 Islands",
-                URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_LTS12Islands/MapServer/tile/{z}/{y}/{x}"
-            },
-            {
-                label: "LTS 3 - Philadelphia County",
-                URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_PhilaLTS3/MapServer/tile/{z}/{y}/{x}"
-            },
-            {
-                label: "LTS 3 - Suburban Counties",
-                URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_SuburbanLTS123/MapServer/tile/{z}/{y}/{x}"
-            }
-        ]
-    },
-    {
-        group: "Results",
-        items: [
-            {
-                label: "Shortest Path - Philadelphia County",
-                URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_PhilaShortestPath/MapServer/tile/{z}/{y}/{x}"
-            },
-            {
-                label: "Shortest Path - Suburban Counties",
-                URL: "https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_SuburbanShortestPath/MapServer/tile/{z}/{y}/{x}"
-            }
-        ]
-    },
-];
+function _parseOverlayGroups(base, fn, args) {
+    var retvals = [];
+    for (var i in base) {
+        retvals[i] = fn(base[i], args);
+    }
+    return retvals;
+}
+
+function _parseOverlayItems(base, fn, args) {
+    var _fn = function(grp, args) {
+        var retvals = [];
+        for (var i in grp.items) {
+            retvals[i] = fn(grp.items[i], args);
+        }
+        return retvals;
+    };
+    return _parseOverlayGroups(base, _fn);
+}
+
+function _initOverlay() {
+    var fn = function(item, args) {
+        return item.label;
+    };
+    return _parseOverlayItems(overlays.definition, fn);
+}
 
 function _formatOverlayLayerControl() {
     var flatLayers = [];
-    for (var i0 in overlays) {
-        var grp = overlays[i0];
+    for (var i0 in overlays.definition) {
+        var grp = overlays.definition[i0];
         for (var i1 in grp.items) {
             var item = grp.items[i1];
             flatLayers[item.label] = item.URL;
@@ -91,19 +118,15 @@ function main() {
     var tileLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
     });
+    _initOverlay();
 
     // var _tl_0 = L.tileLayer('https://arcgis.dvrpc.org/arcgis/rest/services/AppData/AssignedLTS/MapServer/tile/{z}/{y}/{x}', {attribution:"Sean Lawrence"});
     // var _tl_1 = L.tileLayer('https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_LTS12Islands/MapServer/tile/{z}/{y}/{x}', {attribution:"Sean Lawrence"});
     // var _tl_2 = L.tileLayer('https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_PhilaShortestPath/MapServer/tile/{z}/{y}/{x}', {attribution:"Sean Lawrence"});
     // var _tl_3 = L.tileLayer('https://arcgis.dvrpc.org/arcgis/rest/services/AppData/BSTRESS_SuburbanShortestPath/MapServer/tile/{z}/{y}/{x}', {attribution:"Sean Lawrence"});
 
-    // var layerControl = L.control.layers(null, {
-            // "A": _tl_0,
-            // "B": _tl_1,
-            // "C": _tl_2,
-            // "D": _tl_3
-    // });
-    _formatOverlayLayerControl();
+    var layerControl = L.control.layers(null, );
+
 
     map = L.map('map', {
         center: [39.9522, -75.1639],
