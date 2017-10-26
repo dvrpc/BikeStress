4b2f25498ab1b82bbe345d9bf1144712ab592535
wtsay <wtsay@dvrpc.org>
2017-10-26 10:57:53 -0400

    Index

---
+++ htdocs/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!-- Handcrafted, locally sourced sustainable free-range HTML -->
+<html>
+    <head>
+        <title>?</title>
+        <!-- Bootstrap CSS -->
+        <!-- <link rel="stylesheet" href="bootstrap.min.css"> -->
+        <!-- <link rel="stylesheet" href="bootstrap-theme.min.css"> -->
+
+        <!-- Jquery UI CSS -->
+        <!-- <link rel="stylesheet" href="jquery-ui.min.css"> -->
+        <!-- <link rel="stylesheet" href="jquery-ui.structure.min.css"> -->
+        <!-- <link rel="stylesheet" href="jquery-ui.theme.min.css"> -->
+
+        <!-- Leaflet CSS -->
+        <link rel="stylesheet" href="leaflet.css" />
+
+        <!-- ? -->
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <div id="box">
+            <div id="left" class="col-md-3">
+            </div>
+            <div id="map" class="col-md-9"></div>
+        </div>
+
+        <!-- JQuery -->
+        <!-- <script src="js/lib/jquery-3.2.1.min.js"></script> -->
+        <!-- <script src="js/jquery-ui.min.js"></script> -->
+
+        <!-- Bootstrap -->
+        <!-- <script src="js/lib/bootstrap.min.js"></script> -->
+
+        <!-- Leaflet -->
+        <script src="js/lib/leaflet.js"></script>
+
+        <!-- ? -->
+        <script src="js/index.js"></script>
+    </body>
+</html>
