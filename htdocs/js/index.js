c0f13cfcc971ffda8757709c36df53cf1e041864
wtsay <wtsay@dvrpc.org>
2017-10-26 14:11:26 -0400

    The Creeper

--- htdocs/js/index.js
+++ htdocs/js/index.js
@@ -14,6 +14,10 @@ var paperUSLetterL = {
     name: 'Landscape'
 }
 
+function ESRITileLayer(URL, options) {
+    
+}
+
 function main() {
     var tileLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
---
+++ htdocs/qry/psql/link_name_location.sql
@@ -0,0 +1,29 @@
+SELECT
+    _q.name,
+    _q.county_code,
+    tim23_area.name AS county_name,
+    _q.cnt,
+    _q.geojson
+FROM (
+    SELECT
+        name,
+        cnt,
+        county_code,
+        ST_AsGeoJSON(ST_Centroid(ST_Collect(multipoint)), 6) AS geojson
+    FROM (
+        SELECT
+            LOWER(name) AS name,
+            county_code,
+            COUNT(*) AS cnt,
+            array_agg(ST_Centroid(tim23_link.geom)) AS multipoint
+        FROM link
+        LEFT JOIN tim23_link
+        ON link.fromnodeno = tim23_link.fromnodeno
+        AND link.tonodeno = tim23_link.tonodeno
+        GROUP BY name, county_code
+        ORDER BY name
+    ) AS _q0
+) AS _q
+LEFT JOIN tim23_area
+ON _q.county_code = tim23_area.no
+WHERE tim23_area.name IS NOT NULL
