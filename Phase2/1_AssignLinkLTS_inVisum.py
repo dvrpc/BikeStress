71a54b2fe42998f8e98a039d72734daffb971ef6
smoran <smoran@dvrpc.org>
2019-01-03 19:19:38 -0500

    phase 2 lts assignment
    and troubleshooting edited network

---
+++ Phase2/1_AssignLinkLTS_inVisum.py
@@ -0,0 +1,174 @@
+import numpy
+import VisumPy.helpers as h
+import VisumPy.matrices as m
+import csv
+import os
+
+Visum = h.CreateVisum(15)
+# drag in version file
+#D:\BikePedTransit\BikeStress\versionfiles\Phase2_NJEdits\Phase2_Network_010219.ver
+
+#create SpeedToUse UDA and set to VCur_PrTSyS(Car)
+
+# grab Link attributes
+### make sure all these link attributes are populated, especially in newly added trail links
+FromNode         = h.GetMulti(Visum.Net.Links, r"FromNodeNo")
+ToNode           = h.GetMulti(Visum.Net.Links, r"ToNodeNo")
+Length           = h.GetMulti(Visum.Net.Links, r"Length")
+NumLanes         = h.GetMulti(Visum.Net.Links, r"NumLanes")
+BikeFac          = h.GetMulti(Visum.Net.Links, r"BIKE_FAC")
+Speed            = h.GetMulti(Visum.Net.Links, r"SPEEDTOUSE")
+OneWay           = h.GetMulti(Visum.Net.Links, r"ISONEWAY")
+LinkType         = h.GetMulti(Visum.Net.Links, r"TypeNo")
+
+TotLanes = [0] * len(FromNode)
+#if one way, total number of lanes equals the number of lanes in the lane field
+for i in xrange(0, len(FromNode)):
+    if OneWay[i] == True:
+        TotLanes[i] = NumLanes[i]
+    #if not one way, total number of lanes equals 2 times the number of lanes in the lane field
+    else:
+        TotLanes[i] = NumLanes[i] * 2
+
+#create lookup for lanes and speed columns in Figure 1 in paper
+#second residential line modified to include speed up to 36 to make sure all fit into that category
+road_index = [
+    [(0, 0),   (0,  999)],
+    [(-2, -2), (0,  25 )], 
+    [(-2, -2), (26, 36 )], 
+    [(1, 3),   (0,  25 )],
+    [(4, 5),   (0,  25 )],
+    [(1, 3),   (26, 34 )],
+    [(6, 999), (0,  25 )],
+    [(4, 5),   (26, 34 )],
+    [(6, 999), (26, 34 )],
+    [(1, 3),   (35, 999)],
+    [(4, 5),   (35, 999)],
+    [(6, 999), (35, 999)],
+]
+
+residential_index = [
+    [(0, 0),   (0,  999)],
+    [(1, 2),   (0,  25 )], 
+    [(1, 2),   (26, 65 )], 
+]
+
+
+#create lookup for bike facility column in stress table
+#numbers re-ordered to act as crosswalk between model bike fac codes and those in the reduction factor table from the paper
+bikeFac_index = [0, 5, 1, 2, 3, 4, 9]
+
+#from figure 1 in paper
+#row 0 is filler for roads with no lanes
+#column 7 is filler for roads with bike fac  =  (opposite direction of a one way street)
+ReducFactorTbl = [
+    [-1,   -1,   -1,   -1,  -1,   -1,   -1],
+    [0.10, 0.10, 0.09, 0.05, 0.04, 0.03,-2],
+    [0.15, 0.14, 0.14, 0.08, 0.05, 0.04,-2],
+    [0.20, 0.19, 0.18, 0.10, 0.07, 0.05,-2],
+    [0.35, 0.33, 0.32, 0.18, 0.12, 0.09,-2],
+    [0.40, 0.38, 0.36, 0.20, 0.14, 0.10,-2],
+    [0.67, 0.64, 0.60, 0.34, 0.23, 0.17,-2],
+    [0.70, 0.67, 0.63, 0.35, 0.25, 0.18,-2],
+    [0.80, 0.76, 0.72, 0.40, 0.28, 0.20,-2],
+    [1.00, 0.95, 0.90, 0.50, 0.35, 0.25,-2],
+    [1.20, 1.14, 1.08, 0.60, 0.42, 0.30,-2],
+    [1.40, 1.33, 1.26, 0.70, 0.49, 0.35,-2],
+]
+
+#function to identify the row of Figure 1 in paper that the record falls in based on the total number of lanes and the speed
+def findRowIndex(lanes, spd, link_type):
+    if link_type in (72, 79) and lanes in (1, 2):
+        for i, ((minlanes,maxlanes), (lowerspd, upperspd)) in enumerate(residential_index):
+            if (minlanes <= lanes and lanes <= maxlanes) and (lowerspd <= spd and spd <= upperspd):
+                return i
+    else:
+        for i, ((minlanes,maxlanes), (lowerspd, upperspd)) in enumerate(road_index):
+            if (minlanes <= lanes and lanes <= maxlanes) and (lowerspd <= spd and spd <= upperspd):
+                return i
+
+#function to idenfity the colum of Figure 1 based on the bicycle facility
+def bikeFacLookup(fac_code):
+    for i, (facility) in enumerate(bikeFac_index):
+        if facility == fac_code:
+            return i
+    
+
+LinkStress = [0]* len(FromNode)
+       
+for i in xrange(0, len(FromNode)):
+	x = bikeFacLookup(BikeFac[i])
+	y = findRowIndex(TotLanes[i], Speed[i], LinkType[i])
+	LinkStress[i] = ReducFactorTbl[y][x]
+    
+ 
+#for troubleshooting if needed
+##view from/to node nos to search for links that are causing errors so they can be fixed (only 12 in this round - mostly related to newly split links missing attributes)
+x_error_rows = []
+y_error_rows = []
+for i in xrange(0, len(FromNode)):
+    x = bikeFacLookup(BikeFac[i])
+    y = findRowIndex(TotLanes[i], Speed[i], LinkType[i])
+    if x is None:
+        x_error_rows.append(i)
+    if y is None:
+        y_error_rows.append(i)
+  
+x_pairs=[]
+for i in xrange(0, len(x_error_rows)):
+    x_pairs.append(str(FromNode[x_error_rows[i]]) + ', ' +str(ToNode[x_error_rows[i]]))
+    
+y_pairs=[]
+for i in xrange(0, len(y_error_rows)):
+    y_pairs.append(str(FromNode[y_error_rows[i]]) + ', ' +str(ToNode[y_error_rows[i]]))
+ 
+
+#write into UDA field in Visum
+h.SetMulti(Visum.Net.Links, "LinkLTS", LinkStress)
+#save version file
+
+#create df and dump into DB
+
+
+'''            
+#write out to join to shapefile in ArcGIS (if needed)            
+#create file and open for writing as the for loop iterates
+with open(r'\\SMORAN\dvrpc_shared\PythonReference\BikeStressMap\output_data\LTS_output_test.csv','wb') as IO:
+    w = csv.writer(IO)
+    #write the header row
+    w.writerow(['FromNode', 'ToNode', 'NodeCode', 'LinkLTS', 'OneWay'])    
+    #for each row (ID), use the lookup table to find the LTS value
+    #also concatenate from node and to node to create nodecode field
+    for i in xrange(0, len(FromNode)):
+        x = bikeFacLookup(BikeFac[i])
+        y = findRowIndex(TotLanes[i], Speed[i], LinkType[i])
+        LinkStress = ReducFactorTbl[y][x]
+        w.writerow([FromNode[i], ToNode[i], str(FromNode[i])+str(ToNode[i]), LinkStress[i], OneWay[i]])'''
+
+        
+#OLD
+'''#read csv with link attributes
+with open(r'\\SMORAN\dvrpc_shared\PythonReference\BikeStressMap\input_data\TIM_directed_links_TypeTest.csv','rb') as IO:
+    r = csv.reader(IO)
+    header = r.next() #read header row and move on to next before starting for loop
+    #create array to hold data from csv
+    Links = []
+    for row in r: Links.append(([int(i) for i in row if i]))
+        '''
+#FOR TESTING
+'''
+#create place to put index results
+IDs = []
+values = []
+
+#loop over all rows in the table and send them through the functions to identify the lookup location in the stress table
+for row in Links[0:9]:
+    IDs.append(row[0])
+    x = bikeFacLookup(row[6])
+    y = findRowIndex(row[10], row[7])
+    values.append(stressTbl[y][x])
+    
+#maybe write out to CSV instead...    
+holder = zip(IDs, xvals, yvals)
+holder = zip(IDs, values)
+
---
+++ Phase2/4_NetworkIslandAnalysis.py
@@ -0,0 +1,430 @@
+import psycopg2 as psql
+import csv
+import itertools
+import numpy
+import time
+import sys
+import json
+import scipy.spatial
+import networkx as nx
+
+TBL_ALL_LINKS = "uc_testlinks"
+TBL_CENTS = "uc_testcentroids"
+TBL_LINKS = "ucity_tolerablelinks"
+TBL_NODES = "sa_nodes"
+# TBL_SPATHS = "montco_L3_shortestpaths"
+TBL_TOLNODES = "ucity_tol_nodes"
+TBL_GEOFF_LOOKUP = "geoffs_uc"
+TBL_GEOFF_LOOKUP_GEOM = "geoffs_viageom_uc"
+TBL_MASTERLINKS = "master_links_uc"
+TBL_MASTERLINKS_GEO = "master_links_geo_uc"
+TBL_MASTERLINKS_GROUPS = "master_links_grp_uc"
+
+TBL_GROUPS = "groups_uc"
+TBL_NODENOS = "nodenos_uc"
+TBL_NODES_GEOFF = "nodes_geoff_uc"
+TBL_NODES_GID = "nodes_gid_uc"
+TBL_GEOFF_NODES = "geoff_nodes_uc"
+TBL_OD = "OandD_uc"
+TBL_BLOCK_NODE_GEOFF = "block_node_geoff_uc"
+TBL_GEOFF_GROUP = "geoff_group_uc"
+
+IDX_GEOFF_GROUP = "geoff_group_value_idx_uc"
+IDX_BLOCK_NODE_GEOFF = "block_node_geoff_value_idx_uc"
+IDX_NODENOS = "nodeno_idx_uc"
+IDX_NODES_GEOFF = "nodes_geoff_idx_uc"
+IDX_NODES_GID = "nodes_gid_idx_uc"
+IDX_GEOFF_NODES = "geoff_nodes_idx_uc"
+IDX_OD_value = "od_value_idx_uc"
+
+con = psql.connect(dbname = "BikeStress", host = "localhost", port = 5432, user = "postgres", password = "sergt")
+cur = con.cursor()
+
+
+class Sponge:
+    def __init__(self, *args, **kwds):
+        self._args = args
+        self._kwds = kwds
+        for k, v in kwds.iteritems():
+            setattr(self, k, v)
+        self.code = 0
+        self.groups = {}
+        
+def Group(groupNo, subgraphs, counter):
+    for i, g in enumerate(subgraphs):
+        for (fg, tg) in g.edges():
+            if (fg, tg) in links:
+                links[(fg, tg)].groups[groupNo] = counter
+            if (tg, fg) in links:
+                links[(tg, fg)].groups[groupNo] = counter
+        counter += 1
+    return counter
+
+#grab master links to make graph with networkx
+Q_SelectMasterLinks = """
+    SELECT
+        mixid,
+        fromgeoff,
+        togeoff,
+        cost
+    FROM "{0}";
+    """.format(TBL_MASTERLINKS_GEO)
+cur.execute(Q_SelectMasterLinks)
+#format master links so networkx can read them
+h = ['id','fg','tg','cost']
+links = {}
+for row in cur.fetchall():
+    l = Sponge(**dict(zip(h, row)))
+    links[(l.fg, l.tg)] = l
+
+print time.ctime(), "Creating Graph"
+#create graph
+G = nx.MultiDiGraph()
+for l in links.itervalues():
+    G.add_edge(l.fg, l.tg)
+
+print time.ctime(), "Find Islands"
+counter = 0
+counter = Group(0, list(nx.strongly_connected_component_subgraphs(G)), counter)
+counter = Group(1, list(nx.weakly_connected_component_subgraphs(G)), counter)
+counter = Group(2, list(nx.attracting_component_subgraphs(G)), counter)
+counter = Group(3, list(nx.connected_component_subgraphs(G.to_undirected())), counter)
+
+results = []
+for l in links.itervalues():
+    row = [l.id, l.fg, l.tg]
+    for i in xrange(4):
+        if i in l.groups:
+            row.append(l.groups[i])
+        else:
+            row.append(None)
+    results.append(row)
+
+#get groups back into postgis
+Q_CreateLinkGrpTable = """
+    CREATE TABLE public."{0}"(
+        mixid integer, 
+        fromgeoff integer, 
+        togeoff integer, 
+        strong integer,
+        weak integer,
+        attracting integer,
+        undirected integer
+    );""".format(TBL_GROUPS)
+cur.execute(Q_CreateLinkGrpTable)
+
+str_rpl = "(%s)" % (",".join("%s" for _ in xrange(len(results[0]))))
+arg_str = ','.join(cur.mogrify(str_rpl, x) for x in results)
+
+Q_InsertGrps = """
+INSERT INTO
+    public."{0}"
+VALUES {1}
+""".format(TBL_GROUPS, arg_str)
+
+cur.execute(Q_InsertGrps)
+con.commit()
+del results
+
+#join strong group number to master links geo
+Q_JoinGroupGeo = """
+    CREATE TABLE public."{1}" AS(
+        SELECT * FROM(
+            SELECT 
+                t1.*,
+                t0.strong
+            FROM "{0}" AS t0
+            LEFT JOIN "{2}" AS t1
+            ON t0.mixid = t1.mixid
+        ) AS "{1}"
+    );""".format(TBL_GROUPS, TBL_MASTERLINKS_GROUPS, TBL_MASTERLINKS_GEO)
+cur.execute(Q_JoinGroupGeo)
+con.commit()
+
+# Q_CREATEINDEX = """
+# CREATE INDEX montco_master_links_grp_idx
+   # ON public.montco_master_links_grp (strong ASC NULLS LAST);
+# """
+
+
+#query to find min and max number of strong
+Q_StrongSelect = """
+    SELECT strong FROM "{0}"
+    WHERE strong > 1
+    ;""".format(TBL_MASTERLINKS_GROUPS)
+cur.execute(Q_StrongSelect)
+strong_grps = cur.fetchall()
+
+print time.ctime(), "Create Group Views"
+##iterate over groups
+#Q_CreateView = """CREATE VIEW %s AS(
+#                    SELECT * FROM "{0}"
+#                    WHERE strong = %d)""".format(TBL_MASTERLINKS_GROUPS)
+#for grpNo in xrange(0, max(strong_grps)[0]):
+#    tblname = "links_grp_%d" % grpNo
+#    cur.execute("""DROP VIEW IF EXISTS %s;""" % tblname)
+#    #create view for each group
+#    cur.execute(Q_CreateView % (tblname, grpNo))
+
+#for level 3 analysis
+Q_CreateView = """CREATE VIEW %s AS(
+    SELECT * FROM "{0}"
+    WHERE strong = %d)
+""".format(TBL_MASTERLINKS_GROUPS)
+for grpNo in xrange(min(strong_grps)[0], max(strong_grps)[0]):
+    tblname = "links_uc_grp_%d" % grpNo
+    cur.execute("""DROP VIEW IF EXISTS %s;""" % tblname)
+    #create view for each group
+    cur.execute(Q_CreateView % (tblname, grpNo))
+    
+    
+    
+SQL_GetGeoffs = """SELECT geoffid, vianode, ST_AsGeoJSON(geom) FROM "{0}";""".format(TBL_GEOFF_LOOKUP_GEOM)
+SQL_GetBlocks = """SELECT gid, Null AS dummy, ST_AsGeoJSON(geom) FROM "{0}";""".format(TBL_CENTS)
+
+def GetCoords(record):
+    id, vianode, geojson = record
+    return id, vianode, json.loads(geojson)['coordinates']
+def ExecFetchSQL(SQL_Stmt):
+    cur = con.cursor()
+    cur.execute(SQL_Stmt)
+    return map(GetCoords, cur.fetchall())
+    
+#create OD list
+data = ExecFetchSQL(SQL_GetGeoffs)
+world_ids, world_vias, world_coords = zip(*data)
+node_coords = dict(zip(world_vias, world_coords))
+geoff_nodes = dict(zip(world_ids, world_vias))
+# Node to Geoff dictionary (a 'random' geoff will be selected for each node)
+nodes_geoff = dict(zip(world_vias, world_ids))
+geofftree = scipy.spatial.cKDTree(world_coords)
+del world_coords, world_vias
+
+data = ExecFetchSQL(SQL_GetBlocks)
+results = []
+for i, (id, _, coord) in enumerate(data):
+    dist, index = geofftree.query(coord)
+    geoffid = world_ids[index]
+    nodeno = geoff_nodes[geoffid]
+    results.append((id, nodeno))
+del data, world_ids
+    
+gids, nodenos = zip(*results)
+#nodenos = sorted(set(nodenos))
+# Node to GID dictionary (a 'random' GID will be selected for each node)
+nodes_gids = dict(zip(nodenos, gids))
+nodetree = scipy.spatial.cKDTree(map(lambda nodeno:node_coords[nodeno], nodenos))
+sdm = nodetree.sparse_distance_matrix(nodetree, 8046.72)
+del gids, nodetree, results
+
+OandD = sorted(sdm.keys())
+del sdm
+
+Q_GeoffGroup = """
+WITH geoff_group AS (
+    SELECT
+        fromgeoff AS geoff,
+        strong
+    FROM "{0}"
+    WHERE strong IS NOT NULL
+    GROUP BY fromgeoff, strong
+    
+    UNION ALL
+
+    SELECT
+        togeoff AS geoff,
+        strong
+    FROM "{0}"
+    WHERE strong IS NOT NULL
+    GROUP BY togeoff, strong
+)
+SELECT geoff, strong FROM geoff_group
+GROUP BY geoff, strong
+ORDER BY geoff DESC;
+""".format(TBL_MASTERLINKS_GROUPS)
+
+cur.execute(Q_GeoffGroup)
+geoff_grp_list = cur.fetchall()
+geoff_grp = dict(geoff_grp_list)
+
+CloseEnough = []
+DiffGroup = 0
+NullGroup = 0
+#are the OD geoffs in the same group? if so, add pair to list to be calculated
+for i, (fromnodeindex, tonodeindex) in enumerate(OandD):
+    #if i % pool_size == (worker_number - 1):
+    fromnodeno = nodenos[fromnodeindex]
+    tonodeno = nodenos[tonodeindex]
+    if nodes_geoff[fromnodeno] in geoff_grp and nodes_geoff[tonodeno] in geoff_grp:
+        if geoff_grp[nodes_geoff[fromnodeno]] == geoff_grp[nodes_geoff[tonodeno]]:
+            # if geoff_grp[nodes_geoff[fromnodeno]] == int(sys.argv[1]):
+                CloseEnough.append([
+                    nodes_gids[fromnodeno],    # FromGID
+                    fromnodeno,                # FromNode
+                    nodes_geoff[fromnodeno],   # FromGeoff
+                    nodes_gids[tonodeno],      # ToGID
+                    tonodeno,                  # ToNode
+                    nodes_geoff[tonodeno],     # ToGeoff
+                    geoff_grp[nodes_geoff[fromnodeno]]  # GroupNumber
+                    ])
+        else:
+            DiffGroup += 1
+    else:
+        NullGroup += 1
+
+# write OandD into a table in postgres to refer to later
+# Q_CreateOutputTable = """
+# CREATE TABLE IF NOT EXISTS public."{0}"
+# (
+    # origin integer,
+    # destination integer
+# )
+# WITH (
+    # OIDS = FALSE
+# )
+# TABLESPACE pg_default;
+# COMMIT;
+
+# CREATE INDEX IF NOT EXISTS "{1}"
+    # ON public."{0}" USING btree
+    # (origin, destination)
+    # TABLESPACE pg_default;    
+# """.format(TBL_OD, IDX_OD_value)
+# cur.execute(Q_CreateOutputTable)
+
+# str_rpl = "(%s)" % (",".join("%s" for _ in xrange(len(OandD[0]))))
+# cur.execute("""BEGIN TRANSACTION;""")
+# batch_size = 10000
+# for i in xrange(0, len(OandD), batch_size):
+    # j = i + batch_size
+    # arg_str = ','.join(str_rpl % tuple(map(str, x)) for x in OandD[i:j])
+    # #print arg_str
+    # Q_Insert = """INSERT INTO public."{0}" VALUES {1}""".format(TBL_OD, arg_str)
+    # cur.execute(Q_Insert)
+# cur.execute("COMMIT;")
+
+#convert nodes_geoff, and nodes_gids dictionaries to list and save to tables in postgres
+nodes_geoff_list = [(k, v) for k, v in nodes_geoff.iteritems()]
+nodes_gids_list = [(k, v) for k, v in nodes_gids.iteritems()]
+geoff_nodes_list = [(k, v) for k, v in geoff_nodes.iteritems()]
+
+
+#write these plus nodesnos list into postgres to call later to turn back into a dictionary
+
+
+#write nodenos into a table in postgres to refer to later
+Q_CreateOutputTable = """
+CREATE TABLE IF NOT EXISTS public."{0}"
+(
+    nodes integer
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+COMMIT;
+
+CREATE INDEX IF NOT EXISTS "{1}"
+    ON public."{0}" USING btree
+    (nodes)
+    TABLESPACE pg_default;    
+""".format(TBL_NODENOS, IDX_NODENOS)
+cur.execute(Q_CreateOutputTable)
+
+Q_Insert = """INSERT INTO public."{0}" VALUES (%s)""".format(TBL_NODENOS)
+cur.executemany(Q_Insert, map(lambda v:(v,), nodenos))
+con.commit()
+            
+
+#write node_geoffs into a table in postgres to refer to later
+Q_CreateOutputTable = """
+CREATE TABLE IF NOT EXISTS public."{0}"
+(
+    nodes integer,
+    geoffid integer
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+COMMIT;
+
+CREATE INDEX IF NOT EXISTS "{1}"
+    ON public."{0}" USING btree
+    (nodes, geoffid)
+    TABLESPACE pg_default;    
+""".format(TBL_NODES_GEOFF, IDX_NODES_GEOFF)
+cur.execute(Q_CreateOutputTable)
+
+str_rpl = "(%s)" % (",".join("%s" for _ in xrange(len(nodes_geoff_list[0]))))
+cur.execute("""BEGIN TRANSACTION;""")
+batch_size = 10000
+for i in xrange(0, len(nodes_geoff_list), batch_size):
+    j = i + batch_size
+    arg_str = ','.join(str_rpl % tuple(map(str, x)) for x in nodes_geoff_list[i:j])
+    #print arg_str
+    Q_Insert = """INSERT INTO public."{0}" VALUES {1}""".format(TBL_NODES_GEOFF, arg_str)
+    cur.execute(Q_Insert)
+cur.execute("COMMIT;")
+
+#write node_gids into a table in postgres to refer to later
+Q_CreateOutputTable = """
+CREATE TABLE IF NOT EXISTS public."{0}"
+(
+    nodes integer,
+    gid integer
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+COMMIT;
+
+CREATE INDEX IF NOT EXISTS "{1}"
+    ON public."{0}" USING btree
+    (nodes)
+    TABLESPACE pg_default;    
+""".format(TBL_NODES_GID, IDX_NODES_GID)
+cur.execute(Q_CreateOutputTable)
+
+str_rpl = "(%s)" % (",".join("%s" for _ in xrange(len(nodes_gids_list[0]))))
+cur.execute("""BEGIN TRANSACTION;""")
+batch_size = 10000
+for i in xrange(0, len(nodes_gids_list), batch_size):
+    j = i + batch_size
+    arg_str = ','.join(str_rpl % tuple(map(str, x)) for x in nodes_gids_list[i:j])
+    #print arg_str
+    Q_Insert = """INSERT INTO public."{0}" VALUES {1}""".format(TBL_NODES_GID, arg_str)
+    cur.execute(Q_Insert)
+cur.execute("COMMIT;")
+
+#write geoff_nodes into a table in postgres to refer to later
+Q_CreateOutputTable = """
+CREATE TABLE IF NOT EXISTS public."{0}"
+(
+    geoffid integer,
+    nodes integer
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+COMMIT;
+
+CREATE INDEX IF NOT EXISTS "{1}"
+    ON public."{0}" USING btree
+    (nodes, geoffid)
+    TABLESPACE pg_default;    
+""".format(TBL_GEOFF_NODES, IDX_GEOFF_NODES)
+cur.execute(Q_CreateOutputTable)
+
+str_rpl = "(%s)" % (",".join("%s" for _ in xrange(len(geoff_nodes_list[0]))))
+cur.execute("""BEGIN TRANSACTION;""")
+batch_size = 10000
+for i in xrange(0, len(geoff_nodes_list), batch_size):
+    j = i + batch_size
+    arg_str = ','.join(str_rpl % tuple(map(str, x)) for x in geoff_nodes_list[i:j])
+    #print arg_str
+    Q_Insert = """INSERT INTO public."{0}" VALUES {1}""".format(TBL_GEOFF_NODES, arg_str)
+    cur.execute(Q_Insert)
+cur.execute("COMMIT;")
